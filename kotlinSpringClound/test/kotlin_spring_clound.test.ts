483d12235b3d21df317d7be5776a93eb290ab3ba
seongho-joo <hazzyseong@gmail.com>
2022-11-22 19:58:58 +0900

    build: initialize CDK

---
+++ kotlinSpringClound/test/kotlin_spring_clound.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as KotlinSpringClound from '../lib/kotlin_spring_clound-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/kotlin_spring_clound-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new KotlinSpringClound.KotlinSpringCloundStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
