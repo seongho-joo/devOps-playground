0bced95d2f11615f16bbc328117094be37da4774
seongho-joo <hazzyseong@gmail.com>
2022-11-23 19:15:42 +0900

    build: init Spring clound

---
+++ kotlinSpringClound/lambda/command-handlers/src/main/kotlin/com/example/commandhandlers/CommandHandlersApplication.kt
@@ -0,0 +1,11 @@
+package com.example.commandhandlers
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class CommandHandlersApplication
+
+fun main(args: Array<String>) {
+	runApplication<CommandHandlersApplication>(*args)
+}
---
+++ kotlinSpringClound/lambda/command-handlers/src/test/kotlin/com/example/commandhandlers/CommandHandlersApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.commandhandlers
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class CommandHandlersApplicationTests {
+
+	@Test
+	fun contextLoads() {
+	}
+
+}
